c4895a219755d54b3a925aa9aa0badd5c89dcf55
Rajatjha1398 <48334331+Rajatjha1398@users.noreply.github.com>
2019-03-08 02:24:54 +0530

    Update week5i.py

--- Week 5/week5i.py
+++ Week 5/week5i.py
@@ -1,51 +1,64 @@
 #link for question
 #https://onlinecourses.nptel.ac.in/noc19_cs08/progassignment?action=submit&name=108
 
-import sys
+def borrowers_input(b):
+    x=input()
+    while x!='Checkouts':
+        x=x.split('~')
+        b.append(x)
+        x=input()
 
-def sort_key(student):
-    return student[0];
+def checkouts_input(c):
+    x=input()
+    while x!='EndOfInput':
+        x=x.split('~')
+        c.append(x)
+        x=input()
 
-current = 'Courses';
-courses = dict();
-students = dict();
-grades = dict();
-grade_points = {'A':10,'AB':9,'B':8,'BC':7,'C':6,'CD':5,'D':4}
-while(True):
-    line = input();
-    if(line == 'Courses'):
-        current = 'Courses';
-        continue;
-    elif(line == 'Students'):
-        current = 'Students'
-        continue;
-    elif(line == 'Grades'):
-        current = 'Grades';
-        continue;
-    elif(line == 'EndOfInput'):
-        break;
-    elif(current == 'Courses'):
-        code,name,sem,year,instr = line.split('~');
-        courses[code] = (name,sem,year,instr);
-    elif(current == 'Students'):
-        roll,name = line.split('~');
-        students[roll] = name;
-    elif(current == 'Grades'):
-        code,sem,year,roll,grade = line.split('~');
-        try:
-            grades[roll].append((code,sem,year,grade_points[grade]));
-        except:
-            grades[roll] = [(code,sem,year,grade_points[grade])];
+def output():
+    global books,borrower,checkout
+    date=[]
+    uname=[]
+    name=[]
+    Anum=[]
+    title=[]
+    for i in range(0,len(checkout)):
+        date.append(checkout[i][2])
 
-students = sorted(students.items(), key=sort_key);
-for student in students:
-    line = student[0]+'~'+student[1]+'~';
-    points = 0;
-    try:
-        for grade in grades[student[0]]:
-            points += grade[3];
-        points = round(points/len(grades[student[0]]),2)
-    except:
-        pass;
-    line += str(points);
-    print(line);
+    for i in range(0,len(checkout)):
+        uname.append(checkout[i][0])
+
+    for i in range(0,len(uname)):
+        for j in range(0,len(borrower)):
+            if(uname[i] == borrower[j][0]):
+                name.append(borrower[j][1])
+
+    for i in range(0,len(checkout)):
+        Anum.append(checkout[i][1])
+
+    for i in range(0,len(Anum)):
+        for j in range(0,len(books)):
+            if(Anum[i] == books[j][0]):
+                title.append(books[j][1])
+
+    final=[]
+    for i in range(0,len(checkout)):
+        final.append(date[i]+'~'+name[i]+'~'+Anum[i]+'~'+title[i])
+    final.sort()
+    for i in range(0,len(final)):
+        print(final[i])
+
+
+books=[]
+borrower=[]
+checkout=[]
+x=input()
+x=input()
+while x!='Borrowers':
+    x=x.split('~')
+    books.append(x)
+    x=input()
+borrowers_input(borrower)
+borrower.sort()
+checkouts_input(checkout)
+output()
